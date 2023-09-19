8aedaca1add1f3663d084280784e3d0aebdd5db7
Abdullah Al Mamun Sardar <39674487+Almamun809@users.noreply.github.com>
2023-09-19 13:26:08 +0600

    Delete simple_condition.py

--- simple_condition.py
+++
@@ -1,9 +0,0 @@
-num = 7
-if num > 0:
-    print(num, "is a positive number.")
-print("This is always printed.")
-
-num = -6
-if num > 0:
-    print(num, "is a positive number.")
-print("This is also always printed.")
